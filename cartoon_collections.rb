3ddcd804d375f187392559b41fb74d33a6ac4e3b
Zach Parker <parkerz410@gmail.com>
2019-05-21 14:23:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|item| item.include?(cheese_types[])}
+  array.find {|item| item.include?(cheese_types[1..3])}
 end
